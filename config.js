2bad3b44b182c022cb329e9bda588fe1a001a319
LvemiX520 <140702244+LvemiX520@users.noreply.github.com>
2024-05-10 21:55:49 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'LvemiX的网站状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm796893715-edfa21247a6c019a96ab5335',
+    'm784490063-7b5da437e7f1e0d67613714d',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    // {
+    //   text: 'Homepage',
+    //   url: 'https://lvemix520.top/'
+    // },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
